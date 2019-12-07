8dd516f0b6388fe2a50d9c4e32cd9b97c4c9d163
Jason Carver <ut96caarrs@snkmail.com>
2019-12-06 16:10:12 -0800

    New feature only log node hashes present in DB

--- eth/db/accesslog.py
+++ eth/db/accesslog.py
@@ -18,13 +18,21 @@ class KeyAccessLoggerDB(BaseAtomicDB):
 
     keys_read: Set[bytes]
 
-    def __init__(self, wrapped_db: AtomicDatabaseAPI) -> None:
+    def __init__(self, wrapped_db: AtomicDatabaseAPI, log_missing_keys: bool=True) -> None:
         self.wrapped_db = wrapped_db
         self.keys_read = set()
+        self._log_missing_keys = log_missing_keys
 
     def __getitem__(self, key: bytes) -> bytes:
-        self.keys_read.add(key)
-        return self.wrapped_db.__getitem__(key)
+        try:
+            result = self.wrapped_db.__getitem__(key)
+        except KeyError:
+            if self._log_missing_keys:
+                self.keys_read.add(key)
+            raise
+        else:
+            self.keys_read.add(key)
+            return result
 
     def __setitem__(self, key: bytes, value: bytes) -> None:
         self.wrapped_db[key] = value
